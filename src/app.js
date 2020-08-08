b63c8dc937bceed411c923953d11450d42071f46
Sawaijain <dhariwalsawai@gmail.com>
2020-08-08 14:08:29 +0530

    herokku

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ const path = require('path')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+const port = process.env.PORT  || 3000 
+
 app.use(express.static(publicDirectoryPath))
 
 // app.get('/',(req,res)=>{
 //     res.send("<h1>W<h1>")
 // })
 
-app.listen(8080);
-console.log('Express server started');
+// app.listen(8080);
+// console.log('Express server started');
+
+app.listen(port, ()=>{
+    console.log("server is running on port")
+})
